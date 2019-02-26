54b9b573da9eed6e82a9d3ece837d0f54f83ac39
Caique Benassi Bertolozzi <caique.bertolozzi@agv.com.br>
2019-02-26 20:59:39 -0300

    Update site page

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -2,6 +2,119 @@
 
 @section('content')
 <vue-page size="12">
-    
+    <vue-panel title="Articles">
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf45cOeLA9Yq0x80-AVQCq8fh1PiBDC1CcXMrlESj2dXi94YCOmQ" alt="...">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>Description Test</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf45cOeLA9Yq0x80-AVQCq8fh1PiBDC1CcXMrlESj2dXi94YCOmQ" alt="...">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>Description Test</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf45cOeLA9Yq0x80-AVQCq8fh1PiBDC1CcXMrlESj2dXi94YCOmQ" alt="...">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>Description Test</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf45cOeLA9Yq0x80-AVQCq8fh1PiBDC1CcXMrlESj2dXi94YCOmQ" alt="...">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>Description Test</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf45cOeLA9Yq0x80-AVQCq8fh1PiBDC1CcXMrlESj2dXi94YCOmQ" alt="...">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>Description Test</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf45cOeLA9Yq0x80-AVQCq8fh1PiBDC1CcXMrlESj2dXi94YCOmQ" alt="...">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>Description Test</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf45cOeLA9Yq0x80-AVQCq8fh1PiBDC1CcXMrlESj2dXi94YCOmQ" alt="...">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>Description Test</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf45cOeLA9Yq0x80-AVQCq8fh1PiBDC1CcXMrlESj2dXi94YCOmQ" alt="...">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>Description Test</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf45cOeLA9Yq0x80-AVQCq8fh1PiBDC1CcXMrlESj2dXi94YCOmQ" alt="...">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>Description Test</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf45cOeLA9Yq0x80-AVQCq8fh1PiBDC1CcXMrlESj2dXi94YCOmQ" alt="...">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>Description Test</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf45cOeLA9Yq0x80-AVQCq8fh1PiBDC1CcXMrlESj2dXi94YCOmQ" alt="...">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>Description Test</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+                </div>
+            </div>
+        </div>
+    </vue-panel>
 </vue-page>
 @endsection
